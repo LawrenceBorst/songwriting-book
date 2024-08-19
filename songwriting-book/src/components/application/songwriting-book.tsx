b9c68e18f34652c2de06f443556d463db8677340
Lawrence Borst <laurens.s.borst@gail.com>
2024-08-19 20:09:35 +0200

    Initial boilerplate
    Add the StencilJS boilerplate for the whole website

---
+++ songwriting-book/src/components/application/songwriting-book.tsx
@@ -0,0 +1,12 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'songwriting-book',
+  styleUrl: 'songwriting-book.css',
+  shadow: true,
+})
+export class Application {
+  render() {
+    return <div>Hello, World!</div>;
+  }
+}
